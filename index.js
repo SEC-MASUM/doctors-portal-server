a302195fc35fe3110dfc0831a820a4ee4036c973
SEC-MASUM <masum.jess42@gmail.com>
2022-05-17 00:38:13 +0600

    feat : Issue Simple JWT token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 var cors = require("cors");
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
@@ -50,7 +51,12 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
+      res.send({ result, accessToken: token });
     });
 
     // ! Warning
